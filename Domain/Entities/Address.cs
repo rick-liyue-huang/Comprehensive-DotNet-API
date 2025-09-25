b25e09e0e60664aa83b27bc6a4da5e19692318a7
Rick Huang <rick.liyue.huang@gmail.com>
2025-09-25 13:15:54 +1000

    add more class libraries and create Entities

---
+++ Domain/Entities/Address.cs
@@ -0,0 +1,8 @@
+namespace Domain.Entities;
+
+public class Address
+{
+     public string? City { get; set; }
+     public string? Street { get; set; }
+     public string? PostCode { get; set; }
+}
---
+++ Domain/Entities/Restaurant.cs
@@ -0,0 +1,18 @@
+using System.Globalization;
+
+namespace Domain.Entities;
+
+public class Restaurant
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public bool HasDelivery { get; set; }
+    
+    public string? ContactEmail { get; set; }
+    public string? ContactNumber { get; set; }
+    
+    public Address? Address { get; set; }
+    public List<Dish> Dishes { get; set; } = new();
+}
