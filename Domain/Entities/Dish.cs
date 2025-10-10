eb3b181003f2d050b0f717ed5e87da84996fbbd7
Rick Huang <rick.liyue.huang@gmail.com>
2025-10-10 16:18:23 +1100

    add Clean architecture for Domain, Application, Presentation and Infrastrauture

---
+++ Domain/Entities/Dish.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Domain.Entities;
+
+public class Dish
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+}
---
+++ Domain/Entities/Restaurant.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Domain.Entities;
+
+public class Restaurant
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public bool HasDelivery { get; set; }
+    public string? ContactEmail { get; set; }
+    public string? ContactNumber { get; set; }
+
+    public Address? Address { get; set; }
+    public List<Dish> Dishes { get; set; } = new();
+
+
+
+}
